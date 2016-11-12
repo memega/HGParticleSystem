de6cabb8d90f20ec4a5b94417ec3ef7d0c12eec9
Yuriy Panfyorov <tohellwithmemega@gmail.com>
2016-11-13 06:16:23 +0700

    Added framework targets

---
+++ HGParticleSystem/ParticleSystem-ios.h
@@ -0,0 +1,15 @@
+//
+//  ParticleSystem-ios.h
+//  ParticleSystem-ios
+//
+//  Created by Yurii Panforov on 13/11/2016.
+//  Copyright © 2016 Yuriy Panfyorov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <HGParticleSystem/HGParticleSystem.h>
+#import <HGParticleSystem/HGLookupTable.h>
+#import <HGParticleSystem/HGParticleSystemProperty.h>
+#import <HGParticleSystem/HGParticleSystemCache.h>
+
---
+++ HGParticleSystem/ParticleSystem-osx.h
@@ -0,0 +1,15 @@
+//
+//  ParticleSystem-osx.h
+//  ParticleSystem-osx
+//
+//  Created by Yurii Panforov on 13/11/2016.
+//  Copyright © 2016 Yuriy Panfyorov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+#import <HGParticleSystem/HGParticleSystem.h>
+#import <HGParticleSystem/HGLookupTable.h>
+#import <HGParticleSystem/HGParticleSystemProperty.h>
+#import <HGParticleSystem/HGParticleSystemCache.h>
+
